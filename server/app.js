bc78b5cb182a39da60a7bae02decf65bea1db44d
aliciayerinkim <aliciayerinkim@gmail.com>
2026-04-24 21:23:03 -0700

    JSON format error fix

--- server/app.js
+++ server/app.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.json());
-app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "EventHub SJSU server is running." });
