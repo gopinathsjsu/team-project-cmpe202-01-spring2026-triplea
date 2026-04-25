11f5c15c22895bb989675b5a1e7136a5d9b885cc
aliciayerinkim <aliciayerinkim@gmail.com>
2026-04-24 21:35:02 -0700

    duplicate fix

--- server/app.js
+++ server/app.js
@@ -1,19 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const pool = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const eventRoutes = require("./routes/eventRoutes");
-const authRoutes = require("./routes/authRoutes");
 const { errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
 app.use(express.json());
 
 app.get("/", (req, res) => {
