d79eab37a39f1e8538adac5ef2c454f0a3ed22ee
aliciayerinkim <aliciayerinkim@gmail.com>
2026-04-26 18:10:15 -0700

    Protect event APIs with auth middleware (require JWT)

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -4,9 +4,10 @@
 const express = require("express");
 const router = express.Router();
 const { getAllEvents, createEvent, updateEvent } = require("../controllers/eventController");
+const { authenticateToken } = require("../middleware/authMiddleware");
 
 router.get("/", getAllEvents);
-router.post("/", createEvent);
-router.put("/:id", updateEvent);
+router.post("/", authenticateToken, createEvent);
+router.put("/:id", authenticateToken, updateEvent);
 
 module.exports = router;
